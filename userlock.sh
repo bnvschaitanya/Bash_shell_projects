49a590e8911567cba8aa7d4205b5cac0e7de1784
Bellamkonda Narendra Venkata Sai Chaitanya <104776975+bnvschaitanya@users.noreply.github.com>
2024-02-01 21:21:31 +0530

    Update userlock.sh for better readability

--- userlock.sh
+++ userlock.sh
@@ -1,29 +1,37 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-ts=$(date+%F)
+# Generate timestamp
+ts=$(date +%F)
 
-while :
-do
-read -p "Enter the username to lock and backup:" user
+# Infinite loop for user interaction
+while true; do
+    read -p "Enter the username to lock and backup: " user
 
-if [ -z $user ]
-then
-	echo "${user}, user is not provided"
-else
-	if id ${user} $> /dev/null
-	then
-		passwd -l $user
-		homeDir=$(grep ^${user}: /etc/passwd | cut -f 6 -d ":")
-		echo $homeDir
-		if [ -d $homeDir ]
-		then 
-			echo "${homeDir} exsists..."
-			tar -cf ${user}-${ts}.tar $homeDir
-			echo "copy of the file is completed"
-		fi
-		exit 0
-	else
-		echo "provided user $user not exsists..."
-	fi
-fi
+    # Check if the username is provided
+    if [ -z "$user" ]; then
+        echo "Username not provided."
+    else
+        # Check if the user exists
+        if id "$user" &> /dev/null; then
+            # Lock the user account
+            passwd -l "$user"
+
+            # Retrieve user's home directory
+            homeDir=$(grep "^${user}:" /etc/passwd | cut -f 6 -d ":")
+            echo "User's home directory: $homeDir"
+
+            # Check if the home directory exists
+            if [ -d "$homeDir" ]; then 
+                echo "Home directory exists."
+
+                # Create a tar archive of the home directory
+                tar -cf "${user}-${ts}.tar" "$homeDir"
+                echo "Backup completed."
+            fi
+
+            exit 0
+        else
+            echo "User $user does not exist."
+        fi
+    fi
 done
